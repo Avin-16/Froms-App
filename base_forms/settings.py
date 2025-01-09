02ba17dae3a4f0855de3507c4ff58eee9f78920f
Avin-16 <avin4k@gmail.com>
2025-01-09 16:30:41 +0530

    full app setup with database

--- base_forms/settings.py
+++ base_forms/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'froms'
+    'forms',
     'rest_framework'
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'base_forms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Froms_App_DataBase',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- forms/apps.py
+++ forms/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FromsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'froms'
+    name = 'forms'
